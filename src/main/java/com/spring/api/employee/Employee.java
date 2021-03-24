a622b2b44bc02abc15f4939fb4e7b7710cccbe31
kevagreen <kevareynolds@gmail.com>
2021-03-24 07:49:26 -0600

    add @Table annotations

--- src/main/java/com/spring/api/employee/Employee.java
+++ src/main/java/com/spring/api/employee/Employee.java
@@ -1,6 +1,7 @@
 package com.spring.api.employee;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.tomcat.jni.Local;
@@ -13,6 +14,7 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table
+@Builder
 public class Employee {
 
     @Id
