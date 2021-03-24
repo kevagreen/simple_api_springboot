651f460398d46084b540440bcad4b0c94965bd63
kevagreen <kevareynolds@gmail.com>
2021-03-24 07:48:17 -0600

    remove extends from EmployeeService

--- src/main/java/com/spring/api/service/EmployeeService.java
+++ src/main/java/com/spring/api/service/EmployeeService.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface EmployeeService extends CrudRepository<Employee, Long> {
+public interface EmployeeService{
 
     List<Employee> getEmployees();
 }
