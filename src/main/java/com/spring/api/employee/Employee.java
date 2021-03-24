9db6af7d7cb9048aaa714696ff23b3264e1d1d3f
kevagreen <kevareynolds@gmail.com>
2021-03-24 07:38:07 -0600

    update annotations @GeneratedValue

--- src/main/java/com/spring/api/employee/Employee.java
+++ src/main/java/com/spring/api/employee/Employee.java
@@ -5,17 +5,26 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.tomcat.jni.Local;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table
 public class Employee {
 
     @Id
+    @SequenceGenerator(
+            name = "employee_sequence",
+            sequenceName = "employee_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "employee_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
