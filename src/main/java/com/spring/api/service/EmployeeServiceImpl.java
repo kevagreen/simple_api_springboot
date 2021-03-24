99e16168071b03cb41e48ea6fbb3adf4a51a8fc0
kevagreen <kevareynolds@gmail.com>
2021-03-24 09:17:47 -0600

    implement newEmployee method

--- src/main/java/com/spring/api/service/EmployeeServiceImpl.java
+++ src/main/java/com/spring/api/service/EmployeeServiceImpl.java
@@ -1,17 +1,29 @@
 package com.spring.api.service;
 
 import com.spring.api.employee.Employee;
+import com.spring.api.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import java.time.LocalDate;
-import java.time.Month;
+
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
 
+    private final EmployeeRepository employeeRepository;
+
     public List<Employee> getEmployees(){
-        return List.of(
-                new Employee("Kim", "Jones", "kimj@email.com", LocalDate.of(2000, Month.APRIL, 25), 20)
-        );
+        return employeeRepository.findAll();
+
+    }
+
+    public void newEmployee(Employee employee){
+        Optional<Employee> studentOptional = employeeRepository.findByEmail(employee.getEmail());
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("email already taken");
+        }
+        employeeRepository.save(employee);
     }
 }
