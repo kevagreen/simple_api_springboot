e4544c56b18568adf9000e69e1a8990847a6b9f1
kevagreen <kevareynolds@gmail.com>
2021-03-24 09:17:25 -0600

    create newEmployee method

--- src/main/java/com/spring/api/service/EmployeeService.java
+++ src/main/java/com/spring/api/service/EmployeeService.java
@@ -1,11 +1,11 @@
 package com.spring.api.service;
 
 import com.spring.api.employee.Employee;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface EmployeeService{
 
     List<Employee> getEmployees();
+    void newEmployee(Employee employee);
 }
