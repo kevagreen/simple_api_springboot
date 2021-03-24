09d7a7133bc90610754711a9ed9709cbd331e3c5
kevagreen <kevareynolds@gmail.com>
2021-03-24 07:39:28 -0600

    add EmployeeServiceImpl class

---
+++ src/main/java/com/spring/api/service/EmployeeServiceImpl.java
@@ -0,0 +1,17 @@
+package com.spring.api.service;
+
+import com.spring.api.employee.Employee;
+import org.springframework.stereotype.Service;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl {
+
+    public List<Employee> getEmployees(){
+        return List.of(
+                new Employee("Kim", "Jones", "kimj@email.com", LocalDate.of(2000, Month.APRIL, 25), 20)
+        );
+    }
+}
